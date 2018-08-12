119086deb734099bdc8a57c6d2210c12d47452c6
indrajeet.singh <indrajeet.singh@192.168.0.100>
2018-08-13 01:33:51 +0530

    Fixes

---
+++ SpringMVCTutorial/depoly.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker ps -a | grep mvc-tutorial
+if [ $? -eq 0 ]
+then
+	echo "Deleting containers...."
+	docker rm -f mvc-tutorial
+fi
+echo "Creating new..."
+docker run --name mvc-tutorial -d -p 8081:8080 iamisb/mvc-tutorial:latest
